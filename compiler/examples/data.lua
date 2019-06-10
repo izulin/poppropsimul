927fef1ef2690649ff876c669678328bb4958320
Przemek <przemyslaw.uznanski@inf.ethz.ch>
2019-06-10 11:39:33 +0200

    examples simplified

--- compiler/examples/data.lua
+++ compiler/examples/data.lua
@@ -1,9 +1,5 @@
---protokol: krolik doswiadczalny
-
 local states = {1, 2, 3}
 local m = 10
-function foo(q, w)
-  local tmp=w
-  if m>0 then tmp=w+m; m=m-1 end
-  return {q, tmp}
-end
+local tmp=w
+if m>0 then tmp=w+m; m=m-1 end
+return {q, tmp}
--- compiler/examples/majority.lua
+++ compiler/examples/majority.lua
@@ -1,15 +1,13 @@
-local states = {'r', 'g', 'b'}
-function foo(q, w)
-  if q=='g' and w=='r' then return {'b', 'b'} end
-  if (q=='b' or q=='r') and w=='r' then return {'r', 'r'} end
-  if (q=='b' or q=='g') and w=='g' then return {'g', 'g'} end
-  if (q=='g' or q=='r') and w=='b' then return {'b', 'b'} end
-  local tmp = w
-  w=q
-  q=tmp
-  if q=='r' and w=='g' then return {'b', 'b'} end
-  if (q=='b' or q=='r') and w=='r' then return {'r', 'r'} end
-  if (q=='b' or q=='g') and w=='g' then return {'g', 'g'} end
-  if (q=='g' or q=='r') and w=='b' then return {'b', 'b'} end
-  return {w, q}
-end
+states = {'r', 'g', 'b'}
+if q=='g' and w=='r' then return {'b', 'b'} end
+if (q=='b' or q=='r') and w=='r' then return {'r', 'r'} end
+if (q=='b' or q=='g') and w=='g' then return {'g', 'g'} end
+if (q=='g' or q=='r') and w=='b' then return {'b', 'b'} end
+local tmp = w
+w=q
+q=tmp
+if q=='r' and w=='g' then return {'b', 'b'} end
+if (q=='b' or q=='r') and w=='r' then return {'r', 'r'} end
+if (q=='b' or q=='g') and w=='g' then return {'g', 'g'} end
+if (q=='g' or q=='r') and w=='b' then return {'b', 'b'} end
+return {w, q}
